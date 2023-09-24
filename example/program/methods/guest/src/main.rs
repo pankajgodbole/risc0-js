84e5fa0a248f631b188b49cc5e3dbb58e4176a15
Pankaj Godbole <pankajsg@gmail.com>
2023-09-24 18:54:05 +0530

    Added a simple Rust program based on the cryptographic Rust library ring

--- example/program/methods/guest/src/main.rs
+++ example/program/methods/guest/src/main.rs
@@ -1,21 +1,108 @@
-#![no_main]
+// std support is experimental
 // If you want to try std support, also update the guest Cargo.toml file
-#![no_std] // std support is experimental
+//#![no_std]
 
-use risc0_zkvm::guest::env;
+#![no_main]
 
-risc0_zkvm::guest::entry!(main);
+extern crate std;
+extern crate alloc;
+extern crate ring;
+
+use std::{ str, collections::HashMap, num::NonZeroU32 };
+
+use alloc::string::String;
+use alloc::vec::Vec;
+
+use risc0_zkvm::guest::entry;
+
+
+/*
+    Password Database Example
+    https://docs.rs/ring/latest/ring/pbkdf2/index.html#password-database-example
+*/
+
+// v0.17.0-not-released-yet (/home/p/Work/Upwork/Andrew_Cassetti/Code/ring)
+use ring::{digest, pbkdf2};
+
+static PBKDF2_ALG: pbkdf2::Algorithm = pbkdf2::PBKDF2_HMAC_SHA256;
+const CREDENTIAL_LEN: usize = digest::SHA256_OUTPUT_LEN;
+
+pub type Credential = [u8; CREDENTIAL_LEN];
+
+enum Error {
+    WrongUsernameOrPassword
+}
+
+struct PasswordDatabase {
+    pbkdf2_iterations: NonZeroU32,
+    db_salt_component: [u8; 16],
+
+    // Normally this would be a persistent database.
+    storage: HashMap<String, Credential>,
+}
+
+impl PasswordDatabase {
+    pub fn store_password(&mut self, username: &str, password: &str) {
+        let salt = self.salt(username);
+        let mut to_store: Credential = [0u8; CREDENTIAL_LEN];
+        pbkdf2::derive(PBKDF2_ALG, self.pbkdf2_iterations, &salt,
+                       password.as_bytes(), &mut to_store);
+        self.storage.insert(String::from(username), to_store);
+    }
+
+    pub fn verify_password(&self, username: &str, attempted_password: &str)
+                           -> Result<(), Error> {
+        match self.storage.get(username) {
+           Some(actual_password) => {
+               let salt = self.salt(username);
+               pbkdf2::verify(PBKDF2_ALG, self.pbkdf2_iterations, &salt,
+                              attempted_password.as_bytes(),
+                              actual_password)
+                    .map_err(|_| Error::WrongUsernameOrPassword)
+           },
+
+           None => Err(Error::WrongUsernameOrPassword)
+        }
+    }
+
+    // The salt should have a user-specific component so that an attacker
+    // cannot crack one password for multiple users in the database. It
+    // should have a database-unique component so that an attacker cannot
+    // crack the same user's password across databases in the unfortunate
+    // but common case that the user has used the same password for
+    // multiple systems.
+    fn salt(&self, username: &str) -> Vec<u8> {
+        let mut salt = Vec::with_capacity(self.db_salt_component.len() +
+                                          username.as_bytes().len());
+        salt.extend(self.db_salt_component.as_ref());
+        salt.extend(username.as_bytes());
+        salt
+    }
+}
+
+
+entry!(main);
 
 pub fn main() {
-    // Load the first number from the host
-    let a: u64 = env::read();
-    // Load the second number from the host
-    let b: u64 = env::read();
-    // Verify that neither of them are 1 (i.e. nontrivial factors)
-    if a == 1 || b == 1 {
-        panic!("Trivial factors")
-    }
-    // Compute the product while being careful with integer overflow
-    let product = a.checked_mul(b).expect("Integer overflow");
-    env::commit(&product);
+    // Normally these parameters would be loaded from a configuration file.
+    let mut db = PasswordDatabase {
+        pbkdf2_iterations: NonZeroU32::new(100_000).unwrap(),
+        db_salt_component: [
+            // This value was generated from a secure PRNG.
+            0xd6, 0x26, 0x98, 0xda, 0xf4, 0xdc, 0x50, 0x52,
+            0x24, 0xf2, 0x27, 0xd1, 0xfe, 0x39, 0x01, 0x8a
+        ],
+        storage: HashMap::new(),
+    };
+
+    db.store_password("alice", "@74d7]404j|W}6u");
+
+    // An attempt to log in with the wrong password fails.
+    assert!(db.verify_password("alice", "wrong password").is_err());
+
+    // Normally there should be an expoentially-increasing delay between
+    // attempts to further protect against online attacks.
+
+    // An attempt to log in with the right password succeeds.
+    assert!(db.verify_password("alice", "@74d7]404j|W}6u").is_ok());
 }
